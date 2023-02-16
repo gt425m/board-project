b5a4b715b78b8721e278ba983da8fd5ec35dc47e
gt425m <gt425m@gmail.com>
2023-02-16 17:14:21 +0900

    #15 - 게시글 관련 뷰 테스트 작성

---
+++ board-project/src/test/java/com/webexample/boardproject/controller/ArticleControllerTest.java
@@ -0,0 +1,53 @@
+package com.webexample.boardproject.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("view 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("view get 게시글 리스트 - 정상 호출")
+    public void viewListRequestTest() throws Exception {
+        mvc.perform(get("articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("view get 게시글 상세 - 정상 호출")
+    public void viewDetailRequestTest() throws Exception {
+        mvc.perform(get("articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @DisplayName("view get 게시글 검색 - 정상 호출")
+    public void viewSearchRequestTest() throws Exception {
+        mvc.perform(get("articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("view get 게시글 해시태그검색 - 정상 호출")
+    public void viewHashtagSearchRequestTest() throws Exception {
+        mvc.perform(get("articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+}
