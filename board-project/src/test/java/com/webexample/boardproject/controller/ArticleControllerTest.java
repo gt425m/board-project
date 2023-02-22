7ead0f15820f2124f8429f5b19eaa5c88e1a2ec5
gt425m <gt425m@gmail.com>
2023-02-22 14:49:43 +0900

    #15 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영

--- board-project/src/test/java/com/webexample/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/webexample/boardproject/controller/ArticleControllerTest.java
@@ -1,8 +1,10 @@
 package com.webexample.boardproject.controller;
 
+import com.webexample.boardproject.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("view 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
