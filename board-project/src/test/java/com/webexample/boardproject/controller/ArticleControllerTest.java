10c15a4786b12f8eba0daae0ee5c30a225af8da5
gt425m <gt425m@gmail.com>
2023-02-22 10:21:33 +0900

    #15 - 컨트롤러 테스트 수정

--- board-project/src/test/java/com/webexample/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/webexample/boardproject/controller/ArticleControllerTest.java
@@ -33,7 +33,7 @@ class ArticleControllerTest {
     public void viewDetailRequestTest() throws Exception {
         mvc.perform(get("articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -43,7 +43,7 @@ class ArticleControllerTest {
     public void viewSearchRequestTest() throws Exception {
         mvc.perform(get("articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -51,7 +51,7 @@ class ArticleControllerTest {
     public void viewHashtagSearchRequestTest() throws Exception {
         mvc.perform(get("articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 
